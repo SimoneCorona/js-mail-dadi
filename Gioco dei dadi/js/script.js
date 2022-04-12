cece2f54af4afc1f08fa15192782f9303ee48f73
SimoneCorona <corona.simone93@gmail.com>
2022-04-12 15:49:34 +0200

    add esercizio dadi with no winner stamp

---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,19 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+
+// Numero random del user
+const user_num_rand = Math.floor(Math.random() * 6 + 1);
+console.log("Il tuo numero é", user_num_rand);
+// Numero random del computer
+const cpu_num_rand = Math.floor(Math.random() * 6 + 1);
+console.log("Il mio numero é", cpu_num_rand);
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+let winner_game
+    if (user_num_rand = cpu_num_rand) {
+        winner_game = "Abbiamo pareggiato!";
+    } else if (user_num_rand < cpu_num_rand) {
+        winner_game = "Hai perso!";
+    } else (user_num_rand > cpu_num_rand) 
+        winner_game = "Hai vinto!";
+    console.log(winner_game)
+    

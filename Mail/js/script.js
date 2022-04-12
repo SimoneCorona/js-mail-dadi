2f3a68fc9fd85cb8fbd99eabe03d28f8a8cdbe9d
SimoneCorona <corona.simone93@gmail.com>
2022-04-12 16:26:37 +0200

    starting Mail

---
+++ Mail/js/script.js
@@ -0,0 +1,11 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// Chiedi all’utente la sua email,
+const email_user = prompt('Inserisci la tua e-mail');
+console.log(email_user);
+
+// controlla che sia nella lista di chi può accedere,
+const recorded_emails = ["", "", "", "", ""]

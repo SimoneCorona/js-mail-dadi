dd3f4594793b9b89ca056cbd5c8b2ef3775e39d1
SimoneCorona <corona.simone93@gmail.com>
2022-04-12 18:11:53 +0200

    Finish email

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -8,4 +8,21 @@ const email_user = prompt('Inserisci la tua e-mail');
 console.log(email_user);
 
 // controlla che sia nella lista di chi può accedere,
-const recorded_emails = ["", "", "", "", ""]
+const recorded_emails = ["gigino98@gmail.com", "pieroildrago_89@hotmail.it", "dott.piero75@hotmail.it", "spaccaturadimensionale@gmail.com", "rebecca99@hotmail.it"]
+console.log(recorded_emails);
+
+
+let trovata = false;
+for(let i = 0; i < recorded_emails.length; i++) {
+  console.log(recorded_emails[i]);
+  if (email_user === recorded_emails[i]) {
+    trovata = true;
+  }
+}  
+// stampa un messaggio appropriato sull’esito del controllo.
+if (trovata == true) {
+    alert("Accesso effettuato")
+} else {
+ alert("Email non trovata")
+}
+   
